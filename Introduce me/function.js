76c11c32bc0bd5dc2c84bd257690d10c48a37ebb
최준영 <168086573+SKyhumn@users.noreply.github.com>
2025-03-11 20:38:36 +0900

    Update function.js

--- Introduce me/function.js
+++ Introduce me/function.js
@@ -1,4 +1,4 @@
-var typed=new Typed("#typed",{
+let typed=new Typed("#typed",{
     strings:["Front-End Developer", "Back-End Developer", "UI/UX Designer", "Database Engineer", "Web Publisher"],
     typeSpeed: 30,
     backSpeed: 30, 
@@ -18,16 +18,21 @@ window.addEventListener("scroll", function(){
     }
 })
 
+const homeBtn=document.querySelector("header .home");
 const aboutBtn=document.querySelector("header nav .about");
 const featuresBtn=document.querySelector("header nav .features");
 const portfolioBtn=document.querySelector("header nav .portfolio");
 const contactBtn=document.querySelector("header nav .contact");
 
+const homeSection=document.querySelector("#main");
 const aboutSection=document.querySelector("#about");
 const featuresSection=document.querySelector("#features");
 const portfolioSection=document.querySelector("#portfolio");
 const contactSection=document.querySelector("#contact");
 
+homeBtn.addEventListener("click",function(){
+    homeSection.scrollIntoView({behavior:"smooth"});
+})
 aboutBtn.addEventListener("click",function(){
     aboutSection.scrollIntoView({behavior:"smooth"});
 })
